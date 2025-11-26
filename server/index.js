0d114febcfa209be22d85425852275a8ab1e0af2
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-11-26 21:34:05 +0530

    Addes limit in json and  cors also correct cloudname

--- server/index.js
+++ server/index.js
@@ -5,14 +5,22 @@ import authRoute from './routes/auth.Route.js'
 import messageRoutes from "./routes/message.Routes.js";
 import { errorHandler } from './middleware/error.Middleware.js';
 import { connectDB } from './lib/dbConnection.js';
+import cors from 'cors'
 
 dotenv.config();
 const app = express()
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
-app.use(express.json());
-app.use(cookieParser())
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
+app.use(cookieParser());
 
 // Routes
 app.use("/api/auth", authRoute)
--- server/routes/auth.Route.js
+++ server/routes/auth.Route.js
@@ -8,6 +8,7 @@ const router = express.Router()
 router.post("/signup", signupRateLimiter, signup);
 router.post("/login", loginRateLimiter, login);
 router.post("/logout", logout);
+
 router.put("/update-profile", protectRoute, updateProfile);
 router.get("/check", protectRoute, checkAuth);
 
