2f713541f0c534010913cf76ff630e40c3ab9cfe
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-12-02 10:48:49 +0530

    ..

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.use(express.static(clientPath));
 
-  app.get("*", (_, res) => {
+  app.get("/*", (_, res) => {
     res.sendFile(path.join(clientPath, "index.html"));
   });
 }
